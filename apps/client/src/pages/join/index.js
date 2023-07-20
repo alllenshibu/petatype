ff5b0ca59118fab3a4f18b26160f8617aad3233a
Allen Shibu <allenshibu@outlook.in>
2023-07-20 19:38:46 +0530

    Dummy files for lobby

--- apps/client/src/pages/join/index.js
+++ apps/client/src/pages/join/index.js
@@ -1,7 +1,13 @@
 export default function Join() {
     return (
-        <main>
-            <p className="text-3xl font-bold tracking-wider text-neutral-700">Join Lobby</p>
+        <main className="h-screen flex flex-col justify-center items-center gap-10">
+            <div>
+                <p className="text-4xl font-mono">Join a lobby</p>
+            </div>
+            <div className="flex flex-row justify-center items-center gap-4">
+                <input type="text" placeholder="Lobby name" />
+                <button>Join</button>
+            </div>
         </main>
     )
 }
