b34c845b1986d403dd9ab9d4eb305c1d3b37aab8
Allen Shibu <allenshibu@outlook.in>
2023-07-21 00:03:52 +0530

    Create Typer component

--- apps/client/src/components/Typer.js
+++ apps/client/src/components/Typer.js
@@ -1,19 +1,18 @@
 import { use, useEffect, useRef, useState } from "react";
 
-export default function Typer({ text, timer, setTimer, wpm, setWpm, accuracy, setAccuracy }) {
+export default function Typer({ text, timer, setTimer, wpm, setWpm, accuracy, setAccuracy, speedTimeGraph, setSpeedTimeGraph, accuracyTimeGraph, setAccuracyTimeGraph }) {
     const textRef = useRef(null);
-    const [status, setStatus] = useState(0);
     const [textMap, settextMap] = useState([]);
-    const toType = text.split('');
+    const toType = text.split(' ');
     const [index, setIndex] = useState(0);
-
-
-    console.log(toType);
+    const [innerIndex, setinnerIndex] = useState(-1);
 
     useEffect(() => {
         //Create map with letters and color
-        const map = toType.map((letter) => {
-            return { letter: letter, status: -1 };
+        const map = toType.map((word) => {
+            return word.split('').map((letter) => {
+                return { letter: letter, status: -1 };
+            })
         });
         settextMap(map);
         textRef.current.focus();
@@ -30,8 +29,11 @@ export default function Typer({ text, timer, setTimer, wpm, setWpm, accuracy, se
     //to compare text with typed text
     useEffect(() => {
         //compare by text splice
-        handleTextInput();
-    }, [index]);
+        if (innerIndex != -1) {
+            handleTextInput();
+        }
+        console.log(innerIndex, index)
+    }, [innerIndex]);
 
     useEffect(() => {
         computeStats();
@@ -42,19 +44,33 @@ export default function Typer({ text, timer, setTimer, wpm, setWpm, accuracy, se
         let completedLetterCount = 0
         let correctLetterCount = 0
         let incorrectLetterCount = 0
-        let timeRemaining = 30 - timer;
-        textMap.forEach((letter) => {
-            if (letter.status === 1) {
-                completedLetterCount++;
-                correctLetterCount++;
-            } else if (letter.status === 0) {
-                completedLetterCount++;
-                incorrectLetterCount++;
-            }
-            // Number of words completed = (Completed letters / 5)
-            // WPM = number of words completed / minutes elapsed
-            setWpm(Math.round((completedLetterCount / 5) / (timeRemaining / 60)))
-            setAccuracy(Math.round((correctLetterCount / completedLetterCount) * 100))
+        let timeElapsed = 30 - timer;
+        textMap.forEach((word) => {
+            word.forEach((letter) => {
+                if (letter.status === 1) {
+                    completedLetterCount++;
+                    correctLetterCount++;
+                } else if (letter.status === 0) {
+                    completedLetterCount++;
+                    incorrectLetterCount++;
+                }
+            })
+
+        })
+        setWpm(Math.round((completedLetterCount / 5) / (timeElapsed / 60))) // Number of words completed = (Completed letters / 5)           
+        setAccuracy(Math.round((correctLetterCount / completedLetterCount) * 100))         // WPM = number of words completed / minutes elapsed
+
+        let plot = [timeElapsed, wpm];
+        setSpeedTimeGraph((prev) => {
+            const newMap = [...prev];
+            newMap.push(plot);
+            return newMap;
+        })
+        plot = [timeElapsed, accuracy];
+        setAccuracyTimeGraph((prev) => {
+            const newMap = [...prev];
+            newMap.push(plot);
+            return newMap;
         })
     }
 
@@ -62,34 +78,34 @@ export default function Typer({ text, timer, setTimer, wpm, setWpm, accuracy, se
         if (textRef.current.value.length === 0) {
             return;
         }
-        const newText = text.substring(0, index + 1);
-        console.log({ newText });
-        if (newText === textRef.current.value) {
-            setStatus(1);
-            console.log(textMap[index]);
-
-            settextMap((prev) => {
-                const newMap = [...prev];
-                newMap[index].status = 1;
-                return newMap;
-            })
-        } else {
-            setStatus(0);
-            console.log(textMap[index]);
-
-            settextMap((prev) => {
-                const newMap = [...prev];
-                newMap[index].status = 0;
-                return newMap;
-            });
+        for (let i = 0; i <= innerIndex; i++) {
+            if (textRef.current.value.split(' ').slice(-1)[0][i] !== text.split(' ')[index][i]) {
+                settextMap((prev) => {
+                    const newMap = [...prev];
+                    newMap[index][i].status = 0;
+                    return newMap;
+                });
+            }
+            else {
+                settextMap((prev) => {
+                    const newMap = [...prev];
+                    newMap[index][i].status = 1;
+                    return newMap;
+                });
+            }
         }
     }
 
+
     const handleBackSpace = (event) => {
-        if (event.key === "Backspace" && textRef.current.value.length !== 0) {
+        //spacebar
+        if (event.key === " " && textRef.current.value.length !== 0) {
+            setinnerIndex(-1);
+        }
+        if (event.key === "Backspace" && textRef.current.value.length !== 0 && innerIndex !== -1) {
             settextMap((prev) => {
                 const newMap = [...prev];
-                newMap[index].status = -1;
+                newMap[index][innerIndex].status = -1;
                 return newMap;
             });
         } else if (event.ctrlKey && event.key === "Backspace") {
@@ -120,25 +136,35 @@ export default function Typer({ text, timer, setTimer, wpm, setWpm, accuracy, se
                     </select>
                 </div>
             </div>
-            <div id="text-display">{textMap.map((letter) => {
-                var colors = "grey";
-                if (letter.status === 1) {
-                    colors = "green";
-                }
-                else if (letter.status === 0) {
-                    colors = "red";
-                }
-                return <span style={{ color: colors }}>{letter.letter}</span>
-            })}</div>
-
+            <div id="text-display">{textMap.map((word, outerInd) => {
+                return (<span className="indent-3">{word.map((letter, innerInd) => {
+                    var colors = "grey";
+                    var classname;
+                    if (letter.status === 1) {
+                        colors = "white";
+                    }
+                    else if (letter.status === 0) {
+                        colors = "#ED2939";
+                    }
+                    if (innerIndex == -1 && innerInd == 0 && outerInd == index) {
+                        classname = "blinker-before"
+                    }
+                    else if (outerInd == index && innerInd == innerIndex) {
+                        classname = "blinker"
+                    }
+                    return <span className={classname} style={{ color: colors }}>{letter.letter}</span>
+                })} </span>)
+            })}
+            </div>
             <div>
                 <input onBlur={() => {
                     textRef.current.focus();
-                }} tabIndex="0" autoFocus="true" id="user-input" ref={textRef} onKeyDown={handleBackSpace} type="text" placeholder="Start typing..." onChange={() => {
-                    setIndex(textRef.current.value.length - 1);
-                    // if(status===0){
-                    //     textRef.current.value = textRef.current.value.slice(0,textRef.current.value.length-1);
-                    // }
+                }} tabIndex="0" autoFocus="true" id="user-input" ref={textRef} onKeyDown={handleBackSpace} type="text" placeholder="Start typing..." onChange={(event) => {
+                    if (innerIndex != -1 && textRef.current.value.split(' ').slice(-1)[0].length > text.split(' ')[index].length) {
+                        textRef.current.value = textRef.current.value.slice(0, textRef.current.value.length - 1);
+                    }
+                    setIndex(textRef.current.value.split(' ').length - 1);
+                    setinnerIndex(textRef.current.value.split(' ').slice(-1)[0].length - 1) //put 0 index
                 }} />
             </div>
         </>
--- apps/client/src/pages/practice/index.js
+++ apps/client/src/pages/practice/index.js
@@ -1,12 +1,8 @@
+import Typer from "@/components/Typer";
 import { use, useEffect, useRef, useState } from "react";
 
 export default function Practice() {
     const text = "If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play. By inputting the desired number, you can make a list of as many random sentences as you want or need. Producing random sentences can be helpful in a number of different ways.";
-    const textRef = useRef(null);
-    const [textMap, settextMap] = useState([]);
-    const toType = text.split(' ');
-    const [index, setIndex] = useState(0);
-    const [innerIndex, setinnerIndex] = useState(-1);
     const [timer, setTimer] = useState(30);
     const [wpm, setWpm] = useState(0);
     const [accuracy, setAccuracy] = useState(0);
@@ -14,167 +10,9 @@ export default function Practice() {
     const [speedTimeGraph, setSpeedTimeGraph] = useState([]);
     const [accuracyTimeGraph, setAccuracyTimeGraph] = useState([]);
 
-
-    useEffect(() => {
-        //Create map with letters and color
-        const map = toType.map((word) => {
-            return word.split('').map((letter) => {
-                return { letter: letter, status: -1 };
-            })
-        });
-        settextMap(map);
-        textRef.current.focus();
-
-        const interval = setInterval(() => {
-            setTimer((prev) => prev - 1);
-        }, 1000);
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, [])
-
-    //to compare text with typed text
-    useEffect(() => {
-        //compare by text splice
-        if (innerIndex != -1) {
-            handleTextInput();
-        }
-        console.log(innerIndex, index)
-    }, [innerIndex]);
-
-    useEffect(() => {
-        computeStats();
-    }, [timer])
-
-    // Compute WPM and Accuracy
-    const computeStats = () => {
-        let completedLetterCount = 0
-        let correctLetterCount = 0
-        let incorrectLetterCount = 0
-        let timeElapsed = 30 - timer;
-        textMap.forEach((word) => {
-            word.forEach((letter) => {
-                if (letter.status === 1) {
-                    completedLetterCount++;
-                    correctLetterCount++;
-                } else if (letter.status === 0) {
-                    completedLetterCount++;
-                    incorrectLetterCount++;
-                }
-            })
-
-        })
-        setWpm(Math.round((completedLetterCount / 5) / (timeElapsed / 60))) // Number of words completed = (Completed letters / 5)           
-        setAccuracy(Math.round((correctLetterCount / completedLetterCount) * 100))         // WPM = number of words completed / minutes elapsed
-
-        let plot = [timeElapsed, wpm];
-        setSpeedTimeGraph((prev) => {
-            const newMap = [...prev];
-            newMap.push(plot);
-            return newMap;
-        })
-        plot = [timeElapsed, accuracy];
-        setAccuracyTimeGraph((prev) => {
-            const newMap = [...prev];
-            newMap.push(plot);
-            return newMap;
-        })
-    }
-
-    const handleTextInput = () => {
-        if (textRef.current.value.length === 0) {
-            return;
-        }
-        for (let i = 0; i <= innerIndex; i++) {
-            if (textRef.current.value.split(' ').slice(-1)[0][i] !== text.split(' ')[index][i]) {
-                settextMap((prev) => {
-                    const newMap = [...prev];
-                    newMap[index][i].status = 0;
-                    return newMap;
-                });
-            }
-            else {
-                settextMap((prev) => {
-                    const newMap = [...prev];
-                    newMap[index][i].status = 1;
-                    return newMap;
-                });
-            }
-        }
-    }
-
-
-    const handleBackSpace = (event) => {
-        //spacebar
-        if (event.key === " " && textRef.current.value.length !== 0) {
-            setinnerIndex(-1);
-        }
-        if (event.key === "Backspace" && textRef.current.value.length !== 0 && innerIndex !== -1) {
-            settextMap((prev) => {
-                const newMap = [...prev];
-                newMap[index][innerIndex].status = -1;
-                return newMap;
-            });
-        } else if (event.ctrlKey && event.key === "Backspace") {
-            console.log("Ctrl+Backspace");
-            settextMap((prev) => {
-                const newMap = [...prev];
-                newMap.forEach((letter) => {
-                    letter.status = -1;
-                })
-                return newMap;
-            });
-        }
-    }
-
     return (
         <main className="h-screen flex flex-col justify-center items-center gap-10">
-            <div className="flex flex-row justify-evenly items-center gap-10">
-                <div className="flex flex-row justify-center items-center gap-10 text-3xl font-mono">
-                    <p>{timer}</p>
-                    <p>{wpm} WPM</p>
-                    <p>{accuracy & accuracy}%</p>
-                </div>
-                <div>
-                    <select>
-                        <option value="easy">Easy</option>
-                        <option value="medium">Medium</option>
-                        <option value="hard">Hard</option>
-                    </select>
-                </div>
-            </div>
-            <div id="text-display">{textMap.map((word, outerInd) => {
-                return (<span className="indent-3">{word.map((letter, innerInd) => {
-                    var colors = "grey";
-                    var classname;
-                    if (letter.status === 1) {
-                        colors = "white";
-                    }
-                    else if (letter.status === 0) {
-                        colors = "#ED2939";
-                    }
-                    if (innerIndex == -1 && innerInd == 0 && outerInd == index) {
-                        classname = "blinker-before"
-                    }
-                    else if (outerInd == index && innerInd == innerIndex) {
-                        classname = "blinker"
-                    }
-                    return <span className={classname} style={{ color: colors }}>{letter.letter}</span>
-                })} </span>)
-            })}
-            </div>
-            <div>
-                <input onBlur={() => {
-                    textRef.current.focus();
-                }} tabIndex="0" autoFocus="true" id="user-input" ref={textRef} onKeyDown={handleBackSpace} type="text" placeholder="Start typing..." onChange={(event) => {
-                    if (innerIndex != -1 && textRef.current.value.split(' ').slice(-1)[0].length > text.split(' ')[index].length) {
-                        textRef.current.value = textRef.current.value.slice(0, textRef.current.value.length - 1);
-                    }
-                    setIndex(textRef.current.value.split(' ').length - 1);
-                    setinnerIndex(textRef.current.value.split(' ').slice(-1)[0].length - 1) //put 0 index
-                }} />
-            </div>
+            <Typer text={text} timer={timer} setTimer={setTimer} wpm={wpm} setWpm={setWpm} accuracy={accuracy} setAccuracy={setAccuracy} speedTimeGraph={speedTimeGraph} setSpeedTimeGraph={setSpeedTimeGraph} accuracyTimeGraph={accuracyTimeGraph} setAccuracyTimeGraph={setAccuracyTimeGraph} />
         </main>
     )
 }
