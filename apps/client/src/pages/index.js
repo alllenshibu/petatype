29e8c9b2165f3c5cbf70284b51a80b79ed4995fa
Allen Shibu <allenshibu@outlook.in>
2023-07-20 16:31:42 +0530

    Socket works

--- apps/client/src/pages/index.js
+++ apps/client/src/pages/index.js
@@ -1,4 +1,14 @@
+import React, { useEffect } from 'react'
+import socket from '@/configs/socketConfig'
+
 export default function Home() {
+
+  useEffect(() => {
+    socket.on('test', (msg) => {
+      console.log(msg)
+    })
+  }, [])
+
   return (
     <main>
       <p className="text-3xl font-bold tracking-wider text-neutral-700">Petatype</p>
--- apps/client/src/pages/practice/index.js
+++ apps/client/src/pages/practice/index.js
@@ -2,6 +2,9 @@ export default function Practice() {
     return (
         <main>
             <p className="text-3xl font-bold tracking-wider text-neutral-700">Practice</p>
+            <div>
+                <input type="text" placeholder="Start typing..." />
+            </div>
         </main>
     )
 }
