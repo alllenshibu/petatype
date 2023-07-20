77c7d308b36244652d5f82d13903acf37366582c
Allen Shibu <allenshibu@outlook.in>
2023-07-20 19:46:01 +0530

    More styling

--- apps/client/src/pages/index.js
+++ apps/client/src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import socket from '@/configs/socketConfig'
+import Link from 'next/link'
 
 export default function Home() {
 
@@ -10,13 +11,13 @@ export default function Home() {
   }, [])
 
   return (
-    <main>
-      <p className="text-3xl font-bold tracking-wider text-neutral-700">Petatype</p>
-      <div>
-        <button>Practice</button>
-        <button>Solo</button>
-        <button>Create New Lobby</button>
-        <button>Join Lobby</button>
+    <main className='h-screen flex flex-col justify-center items-center gap-10'>
+      <p className="text-5xl">PetaType</p>
+      <div className='flex flex-row gap-4'>
+        <button><Link href="practice">Practice</Link></button>
+        <button><Link href="#">Solo</Link></button>
+        <button><Link href="create">New Lobby</Link></button>
+        <button><Link href="join">Join Lobby</Link></button>
       </div>
     </main>
   )
